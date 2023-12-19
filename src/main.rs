9f5cc30baff20808bbb68aabf0f84fa5fbc83a22
Jacob Lewallen <jlewallen@gmail.com>
2023-12-19 08:09:08 -0800

    Saving work on influxdb import.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 use anyhow::Result;
-use axum::{
-    extract::Path,
-    http::{Method, StatusCode},
-    routing::get,
-    Extension, Json, Router,
-};
 use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Timelike, Utc};
 use chrono_tz::{Tz, US::Pacific};
 
-use http_cache::{CACacheManager, CacheMode, HttpCache};
-use http_cache_reqwest::Cache;
-use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use clap::{Parser, Subcommand};
 use rusqlite::Connection;
 use serde::Serialize;
-use std::{collections::HashMap, sync::Arc};
-use tower_http::{
-    cors::{Any, CorsLayer},
-    trace::{DefaultMakeSpan, TraceLayer},
-};
-use tracing::info;
+use std::collections::HashMap;
 use tracing_subscriber::prelude::*;
 
 mod flickr;
+mod publish;
+mod serve;
+
+#[derive(Serialize)]
+struct Daily {
+    date: DateTime<Utc>,
+    detections: u64,
+}
+
+#[derive(Serialize)]
+struct Hourly {
+    number: u32,
+    time: NaiveTime,
+    detections: u64,
+}
 
 struct BirdDateAndTime {
     utc: DateTime<Utc>,
@@ -463,189 +465,24 @@ impl BirdDb {
     }
 }
 
-#[axum_macros::debug_handler]
-async fn common_name_to_scientific_name() -> Result<Json<HashMap<String, String>>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok(Json(
-        db.common_name_to_scientific_name()
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    ))
-}
-
-#[axum_macros::debug_handler]
-async fn by_common_name() -> Result<Json<Vec<DetectionsByCommonName>>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok(Json(
-        db.by_common_name()
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    ))
-}
-
-#[axum_macros::debug_handler]
-async fn by_day_and_common_name() -> Result<Json<Vec<DetectionsByTimeAndCommonName>>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    Ok(Json(
-        db.by_day_and_common_name()
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-    ))
-}
-
-async fn head_url(url: &str) -> bool {
-    match new_http_client().head(url).send().await {
-        Ok(r) => match r.status() {
-            StatusCode::OK => true,
-            _ => false,
-        },
-        Err(_) => false,
-    }
-}
-
-async fn check_recently_available(file: Recently) -> Recently {
-    let available = head_url(&file.spectrogram_url).await && head_url(&file.audio_url).await;
-    file.into_with_available(available)
-}
-
-async fn check_recentlies_available(files: Vec<Recently>) -> Result<Vec<Recently>> {
-    use futures::StreamExt;
-    use tokio_stream::{self as stream};
-
-    const CONCURRENT_REQUESTS: usize = 5;
-    Ok(stream::iter(files.into_iter())
-        .map(|row| check_recently_available(row))
-        .buffered(CONCURRENT_REQUESTS)
-        .collect::<Vec<_>>()
-        .await)
-}
-
-async fn check_file_available(file: FilesFor) -> FilesFor {
-    let available = head_url(&file.spectrogram_url).await && head_url(&file.audio_url).await;
-    file.into_with_available(available)
-}
-
-async fn check_files_available(files: Vec<FilesFor>) -> Result<Vec<FilesFor>> {
-    use futures::StreamExt;
-    use tokio_stream::{self as stream};
-
-    const CONCURRENT_REQUESTS: usize = 5;
-    Ok(stream::iter(files.into_iter())
-        .map(|row| check_file_available(row))
-        .buffered(CONCURRENT_REQUESTS)
-        .collect::<Vec<_>>()
-        .await)
-}
-
-#[derive(Serialize)]
-struct Daily {
-    date: DateTime<Utc>,
-    detections: u64,
-}
-
-#[derive(Serialize)]
-struct Hourly {
-    number: u32,
-    time: NaiveTime,
-    detections: u64,
-}
-
-#[axum_macros::debug_handler]
-async fn hourly_for(Path(common_name): Path<String>) -> Result<Json<Vec<Hourly>>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let detections = db
-        .hourly_detections(&common_name)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    Ok(Json(detections))
-}
-
-#[axum_macros::debug_handler]
-async fn daily_for(Path(common_name): Path<String>) -> Result<Json<Vec<Daily>>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let detections = db
-        .daily_detections(&common_name)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    Ok(Json(detections))
-}
-
-#[derive(Serialize)]
-struct FilesResponse {
-    detections: DetectionsSummary,
-    files: Vec<FilesFor>,
-}
-
-#[axum_macros::debug_handler]
-async fn files_for(Path(common_name): Path<String>) -> Result<Json<FilesResponse>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let detections = db
-        .summarize_detections(&common_name)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let files = db
-        .files_for(&common_name)
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    let files = check_files_available(files)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    Ok(Json(FilesResponse { detections, files }))
-}
-
-#[derive(Serialize)]
-struct RecentlyResponse {
-    detections: Vec<Recently>,
-}
-
-#[axum_macros::debug_handler]
-async fn recently() -> Result<Json<RecentlyResponse>, StatusCode> {
-    let db = BirdDb::new().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let detections = db
-        .recently()
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    let detections = check_recentlies_available(detections)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    Ok(Json(RecentlyResponse { detections }))
-}
-
-fn new_http_client() -> ClientWithMiddleware {
-    return ClientBuilder::new(reqwest::Client::new())
-        .with(Cache(HttpCache {
-            mode: CacheMode::ForceCache,
-            manager: CACacheManager::default(),
-            options: None,
-        }))
-        .build();
-}
-
-async fn photo_for(Path(common_name): Path<String>) -> Result<Vec<u8>, StatusCode> {
-    let flickr = flickr::FlickrClient::new(
-        &get_flickr_api_key().map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?,
-        new_http_client(),
-    );
-    let mut photos = flickr
-        .search(&common_name)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    match photos.pop() {
-        Some(photo) => Ok(flickr
-            .image(&photo)
-            .await
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?),
-        None => Err(StatusCode::NOT_FOUND),
-    }
-}
-
-struct AppState {}
-
 fn get_rust_log() -> String {
     std::env::var("RUST_LOG").unwrap_or_else(|_| "birbs=info,tower_http=debug".into())
 }
 
 fn get_flickr_api_key() -> Result<String> {
     Ok(std::env::var("FLICKR_API_KEY")?)
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    Serve,
+    Publish(publish::Command),
+}
+
+#[derive(Parser)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Command,
 }
 
 #[tokio::main]
@@ -655,56 +492,10 @@ async fn main() -> Result<()> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let db = BirdDb::new()?;
+    let cli = Cli::parse();
 
-    let _detections = db.detections()?;
-    let _by_common_name = db.by_common_name()?;
-    let _by_day_and_common_name = db.by_day_and_common_name()?;
-    let _common_name_to_scientific_name = db.common_name_to_scientific_name()?;
-    let _files_for = db.files_for("American Crow")?;
-    let _hourly = db.hourly_detections("American Crow")?;
-    let _daily = db.daily_detections("American Crow")?;
-    let _recently = db.recently()?;
-
-    // let flickr = flickr::FlickrClient::new(&get_flickr_api_key()?);
-    // let photos = flickr.search("Chestnut-rumped Thornbill").await?;
-    // use futures::future;
-    // let _photos = future::try_join_all(photos.iter().map(|p| flickr.image(p))).await?;
-
-    let app_state = Arc::new(AppState {});
-
-    let cors = CorsLayer::new()
-        // allow `GET` and `POST` when accessing the resource
-        .allow_methods([Method::GET, Method::POST])
-        // allow requests from any origin
-        .allow_origin(Any);
-
-    let app = Router::new()
-        .route("/", get(|| async { "hello, world!" }))
-        .route(
-            "/common-name-to-scientific-name.json",
-            get(common_name_to_scientific_name),
-        )
-        .route("/recently.json", get(recently))
-        .route("/by-common-name.json", get(by_common_name))
-        .route("/by-day-and-common-name.json", get(by_day_and_common_name))
-        .route("/:common-name/files.json", get(files_for))
-        .route("/:common-name/hourly.json", get(hourly_for))
-        .route("/:common-name/daily.json", get(daily_for))
-        .route("/:common-name/photo.png", get(photo_for))
-        .layer(cors)
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(DefaultMakeSpan::default().include_headers(false)),
-        )
-        .layer(Extension(app_state));
-
-    info!("listening on 0.0.0.0:3100");
-
-    axum::Server::bind(&"0.0.0.0:3100".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-
-    Ok(())
+    match cli.command {
+        Command::Serve => serve::execute().await,
+        Command::Publish(cmd) => publish::execute(cmd).await,
+    }
 }
---
+++ src/publish.rs
@@ -0,0 +1,173 @@
+use anyhow::Result;
+use chrono::{DateTime, NaiveDate, NaiveTime, Utc};
+use clap::Args;
+use futures::stream;
+use influxdb2::models::DataPoint;
+use influxdb2::Client;
+use itertools::Itertools;
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::io::{BufReader, Seek};
+use std::path::Path;
+
+use crate::BirdDateAndTime;
+
+#[derive(Debug, Args)]
+pub struct Command {
+    #[arg(short, long)]
+    watch: bool,
+    file: String,
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+pub async fn execute(cmd: Command) -> Result<()> {
+    let log = BirdLog::new(cmd.file);
+
+    if cmd.watch {
+        log.watch()
+    } else {
+        log.publish_all().await
+    }
+}
+
+pub struct BirdLog {
+    path: String,
+}
+
+impl BirdLog {
+    pub fn new(path: String) -> Self {
+        Self { path }
+    }
+
+    pub fn watch(&self) -> Result<()> {
+        let mut f = std::fs::File::open(&self.path)?;
+        let mut pos = std::fs::metadata(&self.path)?.len();
+
+        let (tx, rx) = std::sync::mpsc::channel();
+        let mut watcher = RecommendedWatcher::new(tx, Config::default())?;
+        watcher.watch(self.path.as_ref(), RecursiveMode::NonRecursive)?;
+
+        for res in rx {
+            match res {
+                Ok(_event) => {
+                    if f.metadata()?.len() == pos {
+                        continue;
+                    }
+
+                    f.seek(std::io::SeekFrom::Start(pos + 1))?;
+
+                    pos = f.metadata()?.len();
+
+                    let reader = BufReader::new(&f);
+                    for line in reader.lines() {
+                        println!("> {:?}", line.unwrap());
+                    }
+                }
+                Err(error) => println!("{error:?}"),
+            }
+        }
+
+        Ok(())
+    }
+
+    fn parse_entry(&self, line: String) -> Result<LogEntry> {
+        let fields = line.split(";").collect_vec();
+
+        let date: NaiveDate = fields[0].parse()?;
+        let time: NaiveTime = fields[1].parse()?;
+        let scientific_name = fields[2].to_owned();
+        let common_name = fields[3].to_owned();
+        let confidence: f64 = fields[4].parse()?;
+        let date_time = BirdDateAndTime::new_naive(date, time)?.into();
+
+        Ok(LogEntry {
+            date_time,
+            common_name,
+            scientific_name,
+            confidence,
+        })
+    }
+
+    pub async fn publish_all(&self) -> Result<()> {
+        let lines = read_lines(&self.path)?;
+
+        let host = std::env::var("INFLUXDB_HOST").unwrap();
+        let org = std::env::var("INFLUXDB_ORG").unwrap();
+        let token = std::env::var("INFLUXDB_TOKEN").unwrap();
+        let client = Client::new(host, org, token);
+
+        if true {
+            for line in lines.skip(1) {
+                if let Ok(line) = line {
+                    let entry = self.parse_entry(line)?;
+
+                    println!("{:?}", entry);
+
+                    let dp: DataPoint = entry.into();
+
+                    client.write("home", stream::iter(vec![dp])).await?;
+                }
+            }
+        } else {
+            let start = NaiveDate::from_ymd_opt(2020, 1, 1)
+                .unwrap()
+                .and_hms_opt(0, 0, 0)
+                .unwrap();
+            let stop = NaiveDate::from_ymd_opt(2024, 1, 1)
+                .unwrap()
+                .and_hms_opt(23, 59, 59)
+                .unwrap();
+
+            client
+                .delete(
+                    "home",
+                    start,
+                    stop,
+                    Some("_measurement=\"birds\"".to_owned()),
+                )
+                .await?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+pub struct LogEntry {
+    date_time: DateTime<Utc>,
+    common_name: String,
+    scientific_name: String,
+    confidence: f64,
+}
+
+pub struct InfluxLineProtocol(String);
+
+impl Into<DataPoint> for LogEntry {
+    fn into(self) -> DataPoint {
+        DataPoint::builder("birds")
+            .tag("station", "backyard")
+            .field(self.common_name, self.confidence)
+            .build()
+            .expect("Error building data point")
+    }
+}
+
+impl Into<InfluxLineProtocol> for LogEntry {
+    fn into(self) -> InfluxLineProtocol {
+        InfluxLineProtocol(format!(
+            "birds,station=backyard {}={} {}",
+            self.common_name.replace(" ", "\\ "),
+            self.confidence,
+            self.date_time.timestamp_nanos()
+        ))
+    }
+}
